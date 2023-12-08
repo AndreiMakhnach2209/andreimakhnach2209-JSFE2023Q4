13e6af4cdb409d000d23088c92a65fcc559c2465
Andrei Makhnach <ahdpyxa88@gmail.com>
2023-12-08 03:46:09 +0300

    feat: implement slider

--- coffee-house/js/slider_about.js
+++ coffee-house/js/slider_about.js
@@ -2,24 +2,97 @@ const slideCards = document.querySelectorAll('.slider__item'),
       slider = document.querySelector('.slider__content'),
       prevBtn = document.querySelector('.slider__btn_left'),
       nextBtn =document.querySelector('.slider__btn_rigth'),
-      numberOfSlides = slideCards.length;
-let slideIndex = 0;
+      numberOfSlides = slideCards.length,
+      slideControls = document.querySelectorAll('.slider__control'),
+      slides = document.querySelectorAll('.slide');
 
-function pagingSlide (index) {
-  let left = (slider.scrollWidth / numberOfSlides) * index;
+let   slideIndex = 0;
+
+function pagingSlide () {
+  let left = (slider.scrollWidth / numberOfSlides) * slideIndex;
   slider.style.left = '-' + left + 'px';
 }
 
 function prevSlide () {
+  clearInterval(fillingControl);
+  progressValue = 0;
+  slideControls[slideIndex].style.backgroundImage = 
+    `linear-gradient(to right, var(--border-dark) 0, var(--border-ligth) 0 100%)`;
   slideIndex = (slideIndex === 0) ? numberOfSlides - 1 : slideIndex - 1;
-  pagingSlide(slideIndex);
+  pagingSlide();
+  pagingInterval();
 }
 
 function nextSlide () {
+  clearInterval(fillingControl);
+  progressValue = 0;
+  slideControls[slideIndex].style.backgroundImage = 
+    `linear-gradient(to right, var(--border-dark) 0, var(--border-ligth) 0 100%)`;
   slideIndex = (slideIndex === numberOfSlides - 1) ? 0 : slideIndex + 1;
-  pagingSlide(slideIndex);
+  pagingSlide();
+  pagingInterval();
 }
 
-pagingSlide(slideIndex);
+pagingSlide();
+window.addEventListener('resize', () => {
+  slider.style.transition = 'none';
+  pagingSlide();
+  slider.style.transition = '0.5s all';
+});
 prevBtn.addEventListener('click', prevSlide);
 nextBtn.addEventListener('click', nextSlide);
+
+let fillingControl,
+    progressValue = 0;
+
+function pagingInterval (value = 0) {
+  fillingControl = setInterval(() => {
+    value++;
+    slideControls[slideIndex].style.backgroundImage = 
+      `linear-gradient(to right, var(--border-dark) ${value}%, ` +
+      `var(--border-ligth) ${value}% 100% )`;
+    if (value === 100) {
+      nextSlide();
+    }
+    progressValue = value;
+  }, 50);
+  
+}
+
+pagingInterval();
+
+let downX, upX;
+
+slides.forEach((item) => {
+  item.addEventListener('pointerover', (event) => {
+    clearInterval(fillingControl);
+    downX = 0;
+    upX = 0;
+  });
+  item.addEventListener('pointerout', () => {
+    pagingInterval(progressValue);
+  });
+
+  item.addEventListener('pointerdown', (event) => {
+    downX = event.clientX;
+    clearInterval(fillingControl);
+    if (navigator.userAgent.toLowerCase().includes('mobile')) {
+      item.addEventListener('pointermove', (event) => {
+        if (event.movementX <= 0) nextSlide();
+        else prevSlide();
+        clearInterval(fillingControl);
+      }, {once: true});
+    }
+  });
+  item.addEventListener('pointerup', (event) => {
+    upX = event.clientX;
+    if (downX > upX) nextSlide();
+    if (downX < upX) prevSlide();
+    clearInterval(fillingControl);
+  });
+  
+
+})
+const userAgent = navigator.userAgent;
+console.log(userAgent);
+
