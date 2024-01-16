f876ef55ce647c54f62e49568976d6f28d466525
Andrei Makhnach <ahdpyxa88@gmail.com>
2024-01-17 01:37:47 +0300

    fix: fix bugs

--- hangman/webpack.config.js
+++ hangman/webpack.config.js
@@ -1,27 +1,4 @@
-const path = require('path')
 
-module.exports = {
-  entry: './src/index.js',
-  module: {
-    rules: [
-      { test: /\.svg$/, use: 'svg-inline-loader' },
-      { test: /\.css$/, 
-        use: [
-          { loader: 'style-loader' },
-          {
-            loader: 'css-loader',
-            options: { modules: true }
-          }
-        ]    
-      },
-      { test: /\.(js)$/, use: 'babel-loader' }
-    ]
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
-}
 const path = require('path')
 
 module.exports = {
