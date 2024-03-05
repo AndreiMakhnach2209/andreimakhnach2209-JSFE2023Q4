80bd27413570ce0b9f0aaa6c4d8312e1311fd979
Andrei Makhnach <ahdpyxa88@gmail.com>
2024-03-05 04:16:01 +0300

    refactor: fix magic numbers

--- news-api/src/components/elements/buttons/burger-btn.ts
+++ news-api/src/components/elements/buttons/burger-btn.ts
@@ -4,7 +4,8 @@ export default function <T extends keyof HTMLElementTagNameMap>(tag: T): HTMLEle
     const container = document.createElement(tag);
     container.id = 'burger_btn';
     container.className = 'burger_btn__container';
-    for (let i = 0; i < 3; i++) {
+    const numberOfLines = 3;
+    for (let i = 0; i < numberOfLines; i++) {
         const line = document.createElement('div');
         line.className = 'burger_btn__line';
         container.append(line);
--- news-api/src/components/elements/buttons/close-button.ts
+++ news-api/src/components/elements/buttons/close-button.ts
@@ -3,8 +3,8 @@ import './close-button.css';
 export default function (tag: string = 'div'): HTMLElement {
     const container = document.createElement(tag);
     container.className = 'close-btn__container';
-
-    for (let i = 0; i < 2; i++) {
+    const numberOfLines = 2;
+    for (let i = 0; i < numberOfLines; i++) {
         const line = document.createElement('div');
         line.className = 'close-btn__line';
         container.append(line);
