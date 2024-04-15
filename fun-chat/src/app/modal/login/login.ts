948bda53fb7d4f0b92dc1cc7242aaa5d7f0b8e26
Andrei Makhnach <ahdpyxa88@gmail.com>
2024-04-16 02:20:04 +0300

    feat: Add validate input for login form

--- fun-chat/src/app/modal/login/login.ts
+++ fun-chat/src/app/modal/login/login.ts
@@ -3,6 +3,7 @@ import Button from '../../../components/button';
 import TextInput from '../../../components/input';
 import createElement from '../../../utilits/createElement';
 import styles from './login.module.scss';
+import showValidityMessage from '../../../utilits/validateInput';
 
 const [nameInput, passwordInput, submitBtn, infobtn] = [
   new TextInput('Имя', 'text'),
@@ -10,6 +11,17 @@ const [nameInput, passwordInput, submitBtn, infobtn] = [
   new Button('Войти', 'submit'),
   new Button('Инфо'),
 ];
+
+nameInput.required = true;
+nameInput.addEventListener('input', showValidityMessage);
+nameInput.minLength = 3;
+nameInput.maxLength = 20;
+
+passwordInput.required = true;
+passwordInput.pattern = '(?=.*[a-zа-я])(?=.*[A-ZА-Я])(?=.*[0-9]).*';
+passwordInput.addEventListener('input', showValidityMessage);
+passwordInput.minLength = 6;
+
 const formLogin = createElement(
   'form',
   [styles.form],
