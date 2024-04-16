d0b7130c58f12c3219de833f6bcdafd5659626a0
Andrei Makhnach <ahdpyxa88@gmail.com>
2024-04-17 02:56:55 +0300

    feat: Define the types for the request to the server

--- fun-chat/src/types/types.ts
+++ fun-chat/src/types/types.ts
@@ -10,8 +10,45 @@ export const enum RequestTypes {
   MSG_EDIT = 'MSG_EDIT',
 }
 
-export interface RequestToServer {
+export const enum ResponseTypes {
+  USER_EXTERNAL_LOGIN = 'USER_EXTERNAL_LOGIN',
+  USER_EXTERNAL_LOGOUT = 'USER_EXTERNAL_LOGOUT',
+  MSG_READED_FROM_SERVER = 'MSG_READED_FROM_SERVER',
+  MSG_DELETED_FROM_SERVER = 'MSG_DELETED_FROM_SERVER',
+  MSG_EDITED_FROM_SERVER = 'MSG_EDITED_FROM_SERVER',
+  MSG_SENDED_FROM_SERVER = 'MSG_SENDED_FROM_SERVER',
+  MSG_DELIVERED = 'MSG_DELIVER',
+  ERROR = 'ERROR',
+}
+
+export interface UserPayload {
+  login: string;
+  password?: string | null;
+  isLogined?: boolean;
+}
+
+export interface MessagePayload {
+  id?: string;
+  from?: string;
+  to: string;
+  text: string;
+  datetime?: number;
+  status?: {
+    isDelivered: boolean;
+    isReaded: boolean;
+    isDeleted: boolean;
+    isEdited: boolean;
+  };
+}
+
+interface BaseRequest {
   id: string | null;
+  payload: { user?: UserPayload; message?: MessagePayload } | null;
+}
+export interface RequestToServer extends BaseRequest {
   type: RequestTypes;
-  payload: Record<string, Record<string, string> | string>;
+}
+
+export interface ResponseFromServer extends BaseRequest {
+  type: RequestTypes | ResponseTypes;
 }
