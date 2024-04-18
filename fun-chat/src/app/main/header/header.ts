3874992c4a8fc9c44ca61e3833205d7faffcde53
Andrei Makhnach <ahdpyxa88@gmail.com>
2024-04-18 22:18:46 +0300

    feat: Implement the header as a class

--- fun-chat/src/app/main/header/header.ts
+++ fun-chat/src/app/main/header/header.ts
@@ -1,20 +1,24 @@
 import Button from '../../../components/button';
-import { RequestTypes, ResponseFromServer } from '../../../types/types';
+import { ResponseFromServer } from '../../../types/types';
 import createElement from '../../../utilits/createElement';
-import Socket from '../../socket/socket';
 import styles from './header.module.scss';
 
-Socket.chat.addEventListener('message', async ({ data }) => {
-  const msg = (await JSON.parse(data)) as ResponseFromServer;
-  if (msg.type === RequestTypes.USER_LOGIN) {
-    const login = msg.payload?.user?.login;
+export default class Header extends HTMLDivElement {
+  constructor(response: ResponseFromServer) {
+    super();
+    const login = response.payload?.user?.login;
     const userName = createElement('span', [styles.userName]);
     userName.textContent = login || null;
     const title = createElement('h1', [styles.title]);
     title.textContent = 'RSS-чат';
     const logoutBtn = new Button('Выйти');
-    document.body.prepend(
-      createElement('header', [styles.header], {}, userName, title, logoutBtn)
-    );
+    this.className = styles.headerInner;
+    this.append(userName, title, logoutBtn);
   }
-});
+
+  public insert() {
+    document.body.prepend(createElement('header', [styles.header], {}, this));
+  }
+}
+
+customElements.define('header-inner', Header, { extends: 'div' });
--- fun-chat/src/app/socket/socket.ts
+++ fun-chat/src/app/socket/socket.ts
@@ -1,3 +1,17 @@
+import { RequestTypes, ResponseFromServer } from '../../types/types';
+import Header from '../main/header/header';
+
 export default class Socket {
   public static chat = new WebSocket('ws://127.0.0.1:4000');
 }
+
+Socket.chat.addEventListener('message', async ({ data }) => {
+  const msg = (await JSON.parse(data)) as ResponseFromServer;
+  switch (msg.type) {
+    case RequestTypes.USER_LOGIN:
+      new Header(msg).insert();
+      break;
+    default:
+      break;
+  }
+});
